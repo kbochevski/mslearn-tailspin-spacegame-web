6bb45ee1f734b94215f6e7f4d3c942622daeca3e
Kaloyan Bochevski <kaloyan.bochevski@ext.ec.europa.eu>
2020-01-24 11:06:03 +0100

    Return correct number of items

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+npm config set proxy http://bocheka:Welcome3044@ps-bxl-usr.cec.eu.int:8012
+npm config set https-proxy http://bocheka:Welcome3044@ps-bxl-usr.cec.eu.int:8012
+
+# Install Node.js modules as defined in package.json.
+npm install --quiet
+
+# Compile Sass (.scss) files to standard CSS (.css).
+node-sass Tailspin.SpaceGame.Web/wwwroot
+
+# Minify JavaScript and CSS files.
+gulp
+
+# Print the date to wwwroot/buildinfo.txt.
+echo `date` > Tailspin.SpaceGame.Web/wwwroot/buildinfo.txt
+
+# Install the latest .NET packages the app depends on.
+dotnet restore
+
+# Build the app under the Debug configuration.
+dotnet build --configuration Debug
+
+# Publish the build to the /tmp directory.
+dotnet publish --no-build --configuration Debug --output /tmp/Debug
+
+# Build the app under the Release configuration.
+dotnet build --configuration Release
+
+# Publish the build to the /tmp directory.
+dotnet publish --no-build --configuration Release --output /tmp/Release
